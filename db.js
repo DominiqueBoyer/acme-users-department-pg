fa73d71451a622c8278ca3f75efe59a2e0856551
a <a@as-iMac.local>
2019-09-14 21:00:04 +0300

    creating the tables and rows

--- db.js
+++ db.js
@@ -3,6 +3,30 @@ const { Client } = pg;
 const client = new Client('postgress://localhost/acme_department');
 client.connect();
 
+const it_uuid = uuid.v4();
+const sales_uuid = uuid.v4();
+const hr_uuid = uuid.v4();
+const marketing_uuid = uuid.v4();
+
+const SQL = `
+DROP TABLE IF EXISTS usrs;
+DROP TABLE IF EXISTS department;
+
+CREATE TABLE department(
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
+  );
+
+  CREATE TABLE user(
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    department_id UUID REFERENCES department(id)
+  );
+
+  INSERT INTO department(id, name) VALUES('${}','IT');
+
+`;
+
 
 module.exports = {
 

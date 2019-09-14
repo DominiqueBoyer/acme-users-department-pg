e215109cb0cb75ef18d2a8f0f80581b898d96c7f
Dominique <dominiqueboyer94@yahoo.com>
2019-09-14 11:18:13 -0700

    got tables to show

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const pg = require('pg');
+const uuid = require('uuid');
 const { Client } = pg;
 const client = new Client('postgress://localhost/acme_department');
 client.connect();
@@ -9,6 +10,10 @@ const hr_uuid = uuid.v4();
 const marketing_uuid = uuid.v4();
 
 const moe_uuid = uuid.v4();
+const larry_uuid = uuid.v4();
+const curly_uuid = uuid.v4();
+const shep_uuid = uuid.v4();
+
 
 const SQL = `
 DROP TABLE IF EXISTS users;
@@ -19,21 +24,28 @@ CREATE TABLE department(
     name VARCHAR(255) UNIQUE NOT NULL
   );
 
-  CREATE TABLE user(
+  CREATE TABLE users(
     id UUID PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL,
     department_id UUID REFERENCES department(id)
   );
 
   INSERT INTO department(id, name) VALUES('${it_uuid}','IT');
-  INSERT INTO department(id, name) VALUES('${sales_uuid}','IT');
-  INSERT INTO department(id, name) VALUES('${hr_uuid}','IT');
-  INSERT INTO department(id, name) VALUES('${marketing_uuid}','IT');
+  INSERT INTO department(id, name) VALUES('${sales_uuid}','SALES');
+  INSERT INTO department(id, name) VALUES('${hr_uuid}','HR');
+  INSERT INTO department(id, name) VALUES('${marketing_uuid}','MARKETING');
 
-  INSERT INTO users(id, name, department_id) VALUES('${it_uuid}','moe', '${it_uuid}');
+  INSERT INTO users(id, name, department_id) VALUES('${moe_uuid}','moe', '${it_uuid}');
+  INSERT INTO users(id, name, department_id) VALUES('${larry_uuid}','larry', '${sales_uuid}');
+  INSERT INTO users(id, name, department_id) VALUES('${curly_uuid}','curly', '${hr_uuid}');
+  INSERT INTO users(id, name) VALUES('${shep_uuid}','shep');
+
 `;
 
+const sync =  async () => {
+  await client.query(SQL);
+  console.log('sucess');
+};
 
-module.exports = {
+sync();
 
-}
